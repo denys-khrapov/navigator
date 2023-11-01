14d47525322d0c2acd6972c905af85403fea42b9
Denys Khrapov <khrapov.denys@gmail.com>
2023-11-01 16:23:36 +0200

    added clients section and styles

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,29 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+jQuery(document).ready(function ($) {
+   initSwiper();
+
+	function initSwiper() {
+      let swiperClients = new Swiper(".slider-clients", {
+         slidesPerView: 'auto',
+         spaceBetween: 16,
+         loop: true,
+			speed: 4000,
+			autoplay: {
+				delay: 300,
+				disableOnInteraction: false,
+			 },
+			freeMode: true,
+         breakpoints: {
+            1280: {
+               spaceBetween: 24,
+            },
+         },
+      });
+
+   }
+
+
+});
